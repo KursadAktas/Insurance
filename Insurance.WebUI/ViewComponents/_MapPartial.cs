e8104e91d6712013f3bbf074cccda3742463e25b
kursa <kursadaktas@yahoo.com>
2023-09-29 13:05:23 +0300

    Add project files.

---
+++ Insurance.WebUI/ViewComponents/_MapPartial.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agriculture.WebUI.ViewComponents
+{
+	public class _MapPartial : ViewComponent
+	{
+		
+		public IViewComponentResult Invoke()
+		{
+			InsuranceContext agricultureContext = new InsuranceContext(); //harita bilgisini viewbag ile taşıyorum.
+			var values = agricultureContext.Adresses.Select(x=>x.MapInfo).FirstOrDefault();
+			ViewBag.v = values;
+			return View();
+		}
+	}
+}
